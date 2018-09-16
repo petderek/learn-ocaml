75b00681ebd190f649585d3bf8fa2f80e91bb122
Derek Petersen <petderek@users.noreply.github.com>
2018-09-16 16:24:20 -0700

    made this one recursive too

--- euler/2.ml
+++ euler/2.ml
@@ -1,11 +1,12 @@
 (* By considering the terms in the Fibonacci sequence whose values do not
 exceed four million, find the sum of the even-valued terms. *)
-let sum = ref 0;;
 
-let data = ref (1,2);;
-while (fst !data) < 4_000_000 do
-  if ((fst !data) mod 2) = 0 then sum := !sum + (fst !data);
-  data := (snd !data), (snd !data) + (fst !data);
-done;;
+let count_evens x =
+  let rec loop t =
+    let first,second = t in
+    if first > x then 0
+    else if (first mod 2) = 0 then first + loop(second, (second+first))
+    else loop(second, (second+first)) in
+  loop (1,2);;
 
-print_endline ("Problem 2: " ^ string_of_int !sum);;
+print_endline ("Problem 2: " ^ string_of_int (count_evens 4_000_000));;
