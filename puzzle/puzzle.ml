2d762644bc1ea3a6e9a7532e0448ddf5ba0873b3
Derek Petersen <petderek@users.noreply.github.com>
2018-09-17 01:00:01 -0700

    insert is working

--- puzzle/puzzle.ml
+++ puzzle/puzzle.ml
@@ -4,25 +4,45 @@ type trie =
     | RootNode of (char * trie) list
     | Empty
 
-let insert trie word = 
-    let firstLetter = String.get word 1 in
+let rec insert (trie:trie) (word:string) = 
     match (String.length word) with
-    | 0 -> ();
+    | 0 -> Node ('q', []);
     | _ -> (
+            let firstLetter = String.get word 0 in
             let isTerminal = String.length word = 1 in
             let remnant = String.sub word 1 ((String.length word)-1) in
-            let node = match List.assoc_opt firstLetter trie with
-                | Some (c,children) as node -> if isTerminal 
-                    then TerminalNode c,children
+            (print_endline ((String.make 1 firstLetter)));
+            let childs: (char * trie) list = match trie with
+                | Node (_, children) -> children
+                | TerminalNode (_, children) -> children
+                | RootNode children -> children
+                | Empty -> [] in
+            let node:trie = match (List.assoc_opt firstLetter childs) with
+                | Some (Node (c,children) as node) -> if isTerminal 
+                    then TerminalNode (c,children)
                     else node
+                | Some (TerminalNode _ as node) -> node
                 | None -> if isTerminal 
-                    then TerminalNode firstLetter, [] 
-                    else Node firstLetter, [] in
-            let newChildren = Node (List.remove_assoc firstLetter) @ (insert node remnant) in
+                    then TerminalNode (firstLetter, [])
+                    else Node (firstLetter, []) in
+            let moddedNode = if remnant = "" then node else (insert node remnant) in
+            let newChildren = (firstLetter, moddedNode) :: (List.remove_assoc firstLetter childs) in
             match trie with 
-                | Node c,_ -> Node c,newChildren
-                | TerminalNode c,_ -> TerminalNode c,newChildren
+                | Node (c, _) -> Node (c, newChildren)
+                | TerminalNode (c, _) -> TerminalNode (c, newChildren)
                 | Empty | RootNode _ -> RootNode newChildren
-           (* TerminalNode firstLetter *  *)
-    )
+    );;
+
+let rec printTrie trie = match trie with
+    | Empty -> "Empty Tree, but heres an emoji: 🍂"
+    | RootNode children -> String.concat ", " (List.map (fun (a,t) -> printTrie t) children)
+    | Node (c, children) -> (String.make 1 c) ^ (String.concat ", " (List.map (fun (a,t) -> printTrie t) children))
+    | TerminalNode (c, children) -> (String.make 1 c) ^ " and its a word" ^ (String.concat ", " (List.map (fun (a,t) -> printTrie t) children));;
+
+
+let tree = insert (insert Empty "helo") "jake";;
+
+print_endline ("Look: " ^ (printTrie tree));;
+
+
 
